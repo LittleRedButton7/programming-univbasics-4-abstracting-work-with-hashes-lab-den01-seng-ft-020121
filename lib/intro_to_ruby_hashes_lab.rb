8a6b40040dbfddd7e749c529be79a9b3336d6872
Marissa Nolan <ritz1484@cs.com>
2021-01-18 23:25:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
 def my_hash_creator(key, value)
-  {:name => "Grace Hopper"}
+  {:key => value}
 end
 
-my_hash_creator
+my_hash_creator("name", "Grace Hopper")
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
